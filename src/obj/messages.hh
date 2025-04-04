9fe277417bd8f5db46f812c49d1336c25fe2af6a
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2025-04-04 11:03:44 +0100

    Defined constant for maximum payload length.

--- src/obj/messages.hh
+++ src/obj/messages.hh
@@ -47,5 +47,6 @@ constexpr unsigned MAX_LABEL_BYTES = 8;
 constexpr unsigned MAX_LABELS = MAX_LABEL_BYTES * 8;
 
 constexpr unsigned MAX_LENGTH_BYTES = 2;
+constexpr unsigned MAX_LENGTH = 65536;
 
 #endif
