6938ccd05b6f1e15ae07847d6a56342a107d3a50
Steven Simpson <568737+simpsonst@users.noreply.github.com>
2025-04-07 11:57:07 +0100

    Logged datagram transmission.

--- src/obj/destinations.cc
+++ src/obj/destinations.cc
@@ -46,6 +46,7 @@
 #include "destinations.hh"
 #include "destruction.hh"
 #include "formatting.hh"
+#include "network.hh"
 
 Destination::Destination(const std::string &name, const YAML::Node &cfg)
   : name(name),
@@ -96,10 +97,39 @@ int Destination::send(int family, int protocol,
 {
   Key key(family, protocol);
   auto pos = options.find(key);
-  if (pos == options.end())
+  if (pos == options.end()) {
+    log.detail([len, pos](std::ostream &out) {
+      out << "sent " << len << " to "
+          << to_str(pos->second.addr(), pos->second.addrlen());
+    });
     return ENOSYS;
+  }
   int rc = ::sendto(sockfd, buf, len, flags,
                     pos->second.addr(), pos->second.addrlen());
-  if (rc == 0) return 0;
-  return errno;
+  if (rc == len) {
+    log.detail([len, pos](std::ostream &out) {
+      out << "sent " << len << " to "
+          << to_str(pos->second.addr(), pos->second.addrlen());
+    });
+    return 0;
+  }
+  if (rc > 0) {
+    log.detail([rc, len, pos](std::ostream &out) {
+      out << "sent " << rc << "<" << len << " to "
+          << to_str(pos->second.addr(), pos->second.addrlen());
+    });
+    /* TODO: What to do here?  Shouldn't be reachable. */
+    return 0;
+  }
+  int ec = errno;
+  log.debug([len, pos, ec](std::ostream &out) {
+    out << "failed to send " << len
+        << " to " << to_str(pos->second.addr(), pos->second.addrlen())
+        << " for " << ec;
+#ifdef WITH_STRERROR_NP
+    out << ":" << strerrorname_np(ec);
+#endif
+    out << " (" << ::strerror(ec) << ")]";
+  });
+  return ec;
 }
